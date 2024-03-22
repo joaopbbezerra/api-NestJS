7a5e8bc7cee1b123ea553e8fb349df2d575b2a6b
João Pedro Bezerra <joaopbbezerra@gmail.com>
2024-03-22 21:28:17 +0000

    feat: Basic crud to understand nestJS decorators

---
+++ src/user/user.controller.ts
@@ -0,0 +1,50 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+
+@Controller("users")
+export class UserController {
+  @Post()
+  async create(@Body() body) {
+    return { body };
+  }
+
+  @Get()
+  async readAll() {
+    return {
+      users: []
+    };
+  }
+
+  @Get(":id")
+  async readOne(@Param() param) {
+    return {
+      user: {},
+      param
+    };
+  }
+
+@Put(':id')
+  async update(@Body() body, @Param() params) {
+    return {
+      method: 'Put',
+      body,
+      params
+    }
+  }
+
+  @Patch(':id')
+  async partialUpdate(@Body() body, @Param() params) {
+    return {
+      method: 'Patch',
+      body,
+      params
+    }
+  }
+
+  @Delete(':id')
+  async deleteOne(@Param() param) {
+    return {
+      method: 'Delete',
+      param
+    }
+  }
+}
