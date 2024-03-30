61afc5218954882b67ca22e1ffe7c53b1ff6fdb7
João Pedro Bezerra <joaopbbezerra@gmail.com>
2024-03-30 17:37:53 +0000

    feat: Enable Cors from localhost:4200

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LogInterceptor());
 
